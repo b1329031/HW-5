7ff4850246e567a9aea42c528b580948e1eca4d0
b1329031 <bobo20060708@gmail.com>
2024-12-11 19:01:18 +0800

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() {
+    int x = 130658;
+    double pi = 0;
+    for(int i = 1; i<=x; i++){
+        if(i%2 == 0){
+            pi -=1.0*4/(1+2*(i-1))*1.0;
+        }else{
+            pi +=1.0*4/(1+2*(i-1))*1.0;
+        }
+    }
+    printf("pi:%.5f x:%d\n",pi, x);
+    return 0;
+}
